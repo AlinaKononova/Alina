d78e45a4fdcbe47cc4b823e550de58a3036afec0
AlinaKononova <54644298+AlinaKononova@users.noreply.github.com>
2021-01-02 15:13:34 +0200

    change #3 functions

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -4,5 +4,17 @@ public class Test {
 
   public static void main(String[] args) {
     System.out.println("Happy New Year!");
+    System.out.println("New York" + " " + "20" + "21");
+    movie("'Девчата'.");
+    double R=5;
+    double P=3.14;
+    System.out.println("Area of circle = " + area(R, P));
+  }
+
+  public static void movie(String something) {
+    System.out.println("One is my favorite movie is" + " " + something);
+  }
+  public static double area(double R, double P){
+    return P * R * R;
   }
 }
